444496ebccc331e80183c2c1b4acb93291967a19
momo07661 <liyuyu23782378@gmail.com>
2025-03-17 13:55:06 +0800

    create GogoApp backend

---
+++ src/main/java/com/example/gogo_backend/GogoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gogo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GogoBackendApplication.class, args);
+	}
+
+}
