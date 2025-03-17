75e36e7347e75cfd9e960add36ae7ad2e6395928
momo07661 <liyuyu23782378@gmail.com>
2025-03-17 14:28:01 +0800

    route modification.

--- src/main/java/com/dev/gogo_backend/GogoBackendApplication.java
+++ src/main/java/com/dev/gogo_backend/GogoBackendApplication.java
@@ -1,4 +1,4 @@
-package com.example.gogo_backend;
+package com.dev.gogo_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/dev/gogo_backend/GogoBackendApplicationTests.java
+++ src/test/java/com/dev/gogo_backend/GogoBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.gogo_backend;
+package com.dev.gogo_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
